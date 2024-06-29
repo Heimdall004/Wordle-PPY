c619d6c0e97f4414cd4600269a94150a1849a9f5
heimdall004 <115761869+Heimdall004@users.noreply.github.com>
2024-06-29 03:40:49 -0400

    Refactor code

---
+++ README.md
@@ -0,0 +1,71 @@
+# Wordle PPY
+
+¡Bienvenido a Wordle PPY! Este es un juego divertido inspirado en Wordle, donde tienes que adivinar una palabra secreta. Cada intento te mostrará qué letras has acertado y cuáles están en la palabra pero en diferentes posiciones. ¡Demuestra tu habilidad con las palabras y diviértete jugando!
+
+> [!IMPORTANT]
+> Este proyecto está en desarrollo continuo. Algunas características pueden cambiar y se pueden agregar nuevas funcionalidades en futuras actualizaciones.
+
+
+## Tabla de Contenidos
+
+- [Características](#características)
+- [Instalación](#instalación)
+- [Uso](#uso)
+- [Contribución](#contribución)
+- [Licencia](#licencia)
+
+## Características
+
+- Interfaz de usuario intuitiva y amigable.
+- Palabras aleatorias de 5 letras para adivinar.
+- Modal informativo sobre el creador del juego y datos curiosos.
+- Posibilidad de jugar de nuevo una vez que termines una partida.
+- Implementación de una API para obtener palabras aleatorias.
+
+## Instalación
+
+1. Clona este repositorio en tu máquina local:
+    ```bash
+    git clone https://github.com/tu-usuario/wordle-ppy.git
+    ```
+
+2. Navega hasta el directorio del proyecto:
+    ```bash
+    cd wordle-ppy
+    ```
+
+3. Abre el archivo `index.html` en tu navegador favorito para empezar a jugar.
+
+## Uso
+
+- **Inicio del juego:** Al cargar la página, se mostrará un modal con una breve descripción del juego. Haz clic en "Inicio juego" para comenzar.
+- **Intentos:** Ingresa una palabra de 5 letras en el campo de texto y presiona el botón "Intentar" o la tecla "Enter".
+- **Reinicio:** Si ganas o pierdes, puedes hacer clic en el botón "Jugar de Nuevo" para reiniciar el juego.
+
+## Estructura del Proyecto
+
+- `index.html` - Contiene la estructura principal del HTML.
+- `assets/` - Carpeta que contiene los archivos de estilo, imágenes y scripts.
+  - `style/` - Contiene los archivos CSS.
+  - `img/` - Contiene las imágenes utilizadas en el proyecto.
+  - `javascript/` - Contiene los archivos JavaScript.
+  - `json/` - Contiene los datos curiosos en formato JSON.
+
+## Contribución
+
+¡Las contribuciones son bienvenidas! Si deseas mejorar este proyecto, por favor sigue estos pasos:
+
+1. Haz un fork del repositorio.
+2. Crea una rama nueva para tu característica (feature) o corrección de errores (bug fix):
+    ```bash
+    git checkout -b nombre-rama
+    ```
+3. Realiza tus cambios y haz commit de ellos:
+    ```bash
+    git commit -m "Descripción de los cambios"
+    ```
+4. Envía tus cambios al repositorio remoto:
+    ```bash
+    git push origin nombre-rama
+    ```
+5. Crea una solicitud de extracción (pull request) en GitHub.
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,163 +1,201 @@
-let intentos = 6;
-let diccionario = ["APPLE", "HURLS", "WINGS", "YOUTH"];
-let palabra = "";
+// Constantes
+const URL_API = "https://random-word.ryanrk.com/api/en/word/random/?length=5";
+const URL_DATOS = "./assets/json/DatosCuriosos.json";
 
-/**
- * Mensajes al Usuario
- */
+const diccionario = [
+    "APPLE",
+    "HURLS",
+    "WINGS",
+    "YOUTH",
+    "BEARS",
+    "CHAIR",
+    "DRAIN",
+    "EAGER",
+    "FAULT",
+    "GHOST",
+    "HOUSE",
+    "IGLOO",
+    "JAZZY",
+    "KNEEL",
+    "LEMON",
+    "MANGO",
+    "NIGHT",
+    "ONION",
+    "PIZZA",
+    "QUICK",
+    "RAVEN",
+    "SALAD",
+    "TABLE",
+    "UNITE",
+    "VIRUS",
+    "WHALE",
+    "XYLOP",
+    "YUMMY",
+    "ZEBRA",
+];
+
+
 const MENSAJES_USER = {
-  MENSAJE_ERROR: "ERROR: El texto no es valido.",
-  MENSAJE_CONEXION: "Conexión exitosa",
-  MENSAJE_ERROR_API: "Conexion fallida con el servidor",
-  MENSAJE_ACIERTO: "<h1>GANASTE!😀</h1>",
-  MENSAJE_PERDISTE: "<h1>PERDISTE!😖</h1>",
+    MENSAJE_ERROR: "ERROR: El texto no es valido.",
+    MENSAJE_CONEXION: "Conexión exitosa",
+    MENSAJE_ERROR_API: "Conexion fallida con el servidor",
+    MENSAJE_ACIERTO: "<h1>GANASTE!😀</h1>",
+    MENSAJE_ACIERTO_UI: `<div class='mensajesJugador'><h3 class='success'>GANASTE!😀</h3></div>`,
+    MENSAJE_PERDISTE: "<h1>PERDISTE!😖</h1>",
+    MENSAJE_PERDISTE_UI: `<div class='mensajesJugador'><h3 class='error'>PERDISTE!😖</h3></div>`,
 };
 
 const COLORS_GRID = {
-  GREEN: "#2ea44f",
-  YELLOW: "#d29922",
-  GREY: "#6e7681",
+    GREEN: "#2ea44f",
+    YELLOW: "#d29922",
+    GREY: "#6e7681",
+};
+const DATOS_MODAL = {
+    TEXT_PARRAFO_INICIO:
+        'Bienvenido a Wordle PPY! Este es un juego divertido donde tienes que adivinar una palabra secreta. Cada intento te mostrará qué letras has acertado y cuáles están en la palabra, pero en diferentes posiciones. ¡Demuestra tu habilidad con las palabras y diviértete jugando! <br> <samp class="warning" >(Este juego es experimental)</samp>',
+    BOTON_CERRAR_MODAL_TEXT: "Inicio juego",
+    IMG_INICIO: "./assets/img/modal.jpeg",
+
+    IMG_CREADOR: "./assets/img/modal3.jpeg",
+    TEXT_PARRAFO_CREADOR:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi illo quod ut fuga, dignissimos unde quas, incidunt ea eos molestiae sint aut necessitatibus. Perspiciatis, illo dolores delectus nemo commodi saepe.",
+    BOTON_CERRAR_MODAL_CREADOR: "Seguir con el Juego",
 };
 
-const URL_DATA ={
-  URL_API : 'https://random-word.ryanrk.com/api/en/word/random/?length=5',
-  URL_DATOS: './assets/json/DatosCuriosos.json'
-}
+// Variables del Juego
+let palabra = "";
+let intentos = 6;
 
-/**
- * Variables del HTML
- */
+// Elementos del DOM
+
+let VENTANA_MODAL = document.getElementById("ventana-modal");
+let IMG_MODAL = document.getElementById("modal-img");
+let MODAL_TEXT_P = document.getElementById("modal-text-p");
+let CERRAR_MODAL = document.getElementById("cerrar-modal");
+
 
 let GRID = document.getElementById("grid");
 let INPUT = document.getElementById("guess-input");
 let CONTENEDOR = document.getElementById("guesses");
 let BUTTON = document.getElementById("guess-button");
 let BUTTON_REINICIO = document.getElementById("guess-button-Reinicio");
-
-let CERRAR_MODAL = document.getElementById('cerrar-modal');
-let VENTANA_MODAL = document.getElementById('ventana-modal');
+let CONTENEDOR_INFO = document.getElementById("contentInfo");
+let CONSTENTDATOS = document.getElementById('contentDatosRandon'); 
 let SPAN_SABIAS = document.getElementById('sabias-que');
 
-
-CERRAR_MODAL.addEventListener('click',()=> VENTANA_MODAL.style.display = 'none');
-CERRAR_MODAL.addEventListener('keydown',(event)=>{
-  if(event.key === 'Enter'){
-    VENTANA_MODAL.style.display = 'none'
-  }
-});
-
-/**
- * Event Listeners y Carga de la Página
- */
-window.addEventListener("load", () => {
-  aleatorioText();
-  sabiasQue();
-  console.info(MENSAJES_USER.MENSAJE_CONEXION);
-});
-
-// Palabra Aleatoria  // Conectar a una API
-function aleatorioText() {
-  fetch(URL_DATA.URL_API)
-    .then((respuesta) => respuesta.json())
-    .then((respuesta) => {
-      palabra = respuesta[0].toUpperCase();
-      console.log(palabra);
-    })
-    .catch((error) => {
-      console.error(MENSAJES_USER.MENSAJE_ERROR_API);
-      palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
-      console.log(palabra);
-    });
-}
-
-// Evento para el botón de intento
+// Evento 
 BUTTON.addEventListener("click", procesarIntento);
-INPUT.addEventListener('keydown',function(event){
-  if(event.key === 'Enter'){
-    procesarIntento();
-  }
-});
-
-
-// Función principal para procesar el intento del usuario
-function procesarIntento() {
-  let textoIngresado = INPUT.value.toUpperCase().trim();
-
-  if (textoIngresado.length === 5) {
-    mostrarIntento(textoIngresado);
-    return;
-  }
-  alert(MENSAJES_USER.MENSAJE_ERROR);
-}
-
-// Función para mostrar el intento del usuario y validar
-function mostrarIntento(text) {
-  let ROW = document.createElement("div");
-  ROW.className = "row";
-
-  for (let i = 0; i < palabra.length; i++) {
-    let SPAN = document.createElement("span");
-    SPAN.className = "letter";
-
-    if (text[i] === palabra[i]) {
-      SPAN.innerHTML = text[i];
-      SPAN.style.backgroundColor = COLORS_GRID.GREEN;
-    } else if (palabra.includes(text[i])) {
-      SPAN.innerHTML = text[i];
-      SPAN.style.backgroundColor = COLORS_GRID.YELLOW;
-    } else {
-      SPAN.innerHTML = text[i];
-      SPAN.style.backgroundColor = COLORS_GRID.GREY;
-    }
-    ROW.appendChild(SPAN);
-  }
-  GRID.appendChild(ROW);
-  intentos--;
-
-  if (text === palabra) {
-    terminarJuego(MENSAJES_USER.MENSAJE_ACIERTO);
-  }
-
-  if (intentos === 0) {
-    terminarJuego(MENSAJES_USER.MENSAJE_PERDISTE);
-  }
-}
-
-// Función para terminar el juego
-function terminarJuego(mensaje) {
-  BUTTON_REINICIO.style.display = "block";
-  INPUT.disabled = true;
-  BUTTON.disabled = true;
-  CONTENEDOR.innerHTML = mensaje;
-}
-
-// Evento para el botón de reinicio
+INPUT.addEventListener("keydown",(event) => event.key === "Enter" && procesarIntento());
 BUTTON_REINICIO.addEventListener("click", nuevoJuego);
+document.getElementById("btn-creador").addEventListener("click", mostrarCreadorModal);
+CERRAR_MODAL.addEventListener("click", () => (VENTANA_MODAL.style.display = "none"));
+window.addEventListener("load",iniciarJuego);
 
-// Función para comenzar un nuevo juego
+// Funciones
+
+function iniciarJuego(){
+    mostrarInicioModal();
+    obtenerPalabraAleatoria();
+    obtenerDatoAleatorio();
+}
+
+function obtenerPalabraAleatoria() {
+    fetch(URL_API)
+        .then((respuesta) => respuesta.json())
+        .then((respuesta) => {
+            palabra = respuesta[0].toUpperCase();
+            console.log(palabra);
+        })
+        .catch((error) => {
+            console.error(MENSAJES_USER.MENSAJE_ERROR_API);
+            palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
+            console.log(palabra);
+        });
+}
+
+function procesarIntento() {
+    let textoIngresado = INPUT.value.toUpperCase().trim();
+    if (textoIngresado.length === 5) {
+        mostrarIntento(textoIngresado);
+        return;
+    }
+    alert(MENSAJES_USER.MENSAJE_ERROR);
+}
+
+function mostrarIntento(text) {
+    let ROW = document.createElement("div");
+    ROW.className = "row";
+
+    for (let i = 0; i < palabra.length; i++) {
+        let SPAN = document.createElement("span");
+        SPAN.className = "letter";
+
+        if (text[i] === palabra[i]) {
+            SPAN.innerHTML = text[i];
+            SPAN.style.backgroundColor = COLORS_GRID.GREEN;
+        } else if (palabra.includes(text[i])) {
+            SPAN.innerHTML = text[i];
+            SPAN.style.backgroundColor = COLORS_GRID.YELLOW;
+        } else {
+            SPAN.innerHTML = text[i];
+            SPAN.style.backgroundColor = COLORS_GRID.GREY;
+        }
+        ROW.appendChild(SPAN);
+    }
+    GRID.appendChild(ROW);
+    intentos--;
+    verificarResultado( text );
+}
+
+function verificarResultado( text ){
+    if (text === palabra) {
+        CONTENEDOR_INFO.innerHTML += MENSAJES_USER.MENSAJE_ACIERTO_UI;
+        terminarJuego(MENSAJES_USER.MENSAJE_ACIERTO);
+    } 
+    else if (intentos === 0) {
+        CONTENEDOR_INFO.innerHTML += MENSAJES_USER.MENSAJE_PERDISTE_UI;
+        terminarJuego(MENSAJES_USER.MENSAJE_PERDISTE);
+    }
+}
+function terminarJuego(mensaje) {
+    BUTTON_REINICIO.style.display = "block";
+    INPUT.disabled = true;
+    BUTTON.disabled = true;
+    CONTENEDOR.innerHTML = mensaje;
+}
 function nuevoJuego() {
-  intentos = 6;
-  limpiarUI();
-  aleatorioText();
-  sabiasQue();
-  BUTTON_REINICIO.style.display = "none";
+    intentos = 6;
+    limpiarUI();
+    obtenerPalabraAleatoria();
+    obtenerDatoAleatorio();
+    BUTTON_REINICIO.style.display = "none";
 }
-
-// Header sabias que? 
-function sabiasQue(){
-  fetch(URL_DATA.URL_DATOS)
-  .then(datosREspuesta => datosREspuesta.json())
-  .then(datos =>{
-    SPAN_SABIAS.innerText = datos[Math.floor(Math.random()*datos.length)].fact;
-  })
-  .catch(error => console.error(error));
-}
-
-// Función para limpiar la interfaz de usuario
 function limpiarUI() {
-  GRID.innerHTML = "";
-  CONTENEDOR.innerHTML = "";
-  INPUT.value = "";
-  INPUT.disabled = false;
-  BUTTON.disabled = false;
+    GRID.innerHTML = "";
+    CONTENEDOR.innerHTML = "";
+    INPUT.value = "";
+    INPUT.disabled = false;
+    BUTTON.disabled = false;
+}
+
+function obtenerDatoAleatorio(){
+    fetch(URL_DATOS)
+    .then(datosREspuesta => datosREspuesta.json())
+    .then(datos =>{
+        SPAN_SABIAS.innerText = datos[Math.floor(Math.random()*datos.length)].fact;
+    })
+    .catch(error => console.error(error));
+}
+
+
+function mostrarInicioModal() {
+    IMG_MODAL.setAttribute("src", DATOS_MODAL.IMG_INICIO);
+    MODAL_TEXT_P.innerHTML = DATOS_MODAL.TEXT_PARRAFO_INICIO;
+    CERRAR_MODAL.innerText = DATOS_MODAL.BOTON_CERRAR_MODAL_TEXT;
+}
+function mostrarCreadorModal() {
+    VENTANA_MODAL.style.display = "block";
+    IMG_MODAL.setAttribute("src", DATOS_MODAL.IMG_CREADOR);
+    MODAL_TEXT_P.innerHTML = DATOS_MODAL.TEXT_PARRAFO_CREADOR;
+    CERRAR_MODAL.innerText = DATOS_MODAL.BOTON_CERRAR_MODAL_CREADOR;
 }
